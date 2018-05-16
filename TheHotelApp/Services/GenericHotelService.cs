fc659c466b45598233237ad277fb995ddf4c285e
John Esan <jcoool40@gmail.com>
2018-05-16 02:10:20 +0100

    Tutorial 2 - Service Layer Added

---
+++ TheHotelApp/Services/GenericHotelService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TheHotelApp.Data;
+
+namespace TheHotelApp.Services
+{
+    public class GenericHotelService<TEntity> : IGenericHotelService<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+        protected DbSet<TEntity> DbSet;
+
+        //GenericHotelService requires a context class to work. 
+        //Constructor sets the DbSet property to the appropraite DbSet representing the Model Entity, ready for use.
+        public GenericHotelService(ApplicationDbContext context)
+        {
+            _context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllItemsAsync()
+        {
+            return await DbSet.ToArrayAsync();
+        }
+
+        public async Task<TEntity> GetItemByIdAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> SearchFor(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.Where(expression).ToArrayAsync();
+        }
+
+        public async Task CreateItemAsync(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditItemAsync(TEntity entity)
+        {
+            _context.Update(entity);
+            //_context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemAsync(TEntity entity)
+        {
+            DbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TheHotelApp/Services/IGenericHotelService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TheHotelApp.Services
+{
+    public interface IGenericHotelService<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllItemsAsync();
+
+        Task<TEntity> GetItemByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> SearchFor(Expression<Func<TEntity, bool>> expression);
+
+        Task CreateItemAsync(TEntity entity);
+
+        Task EditItemAsync(TEntity entity);
+
+        Task DeleteItemAsync(TEntity entity);
+    }
+}
